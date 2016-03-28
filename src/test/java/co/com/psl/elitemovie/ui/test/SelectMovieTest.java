6a0c958f018055a047566a75a476a31fbcb9b4cc
Sebastian Velez <sebasv89@gmail.com>
2016-03-27 19:52:10 -0500

    Fixing timezone detail

--- src/test/java/co/com/psl/elitemovie/ui/test/SelectMovieTest.java
+++ src/test/java/co/com/psl/elitemovie/ui/test/SelectMovieTest.java
@@ -12,47 +12,44 @@ import org.testng.annotations.Test;
 
 import co.com.psl.elitemovie.EliteMovieCleanDataUtils;
 
-public class SelectMovieTest extends EliteMovieTest { 
-	
+public class SelectMovieTest extends EliteMovieTest {
+
 	@BeforeClass
-	public void selectMovieTestBeforeClass()
-	{
+	public void selectMovieTestBeforeClass() {
 		getWebDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
-	
+
 	@Test
 	public void testSelectMovie() {
-		
+
 		getSelectMoviePageBehavior().selectMovieByIndex(1);
-		
-		getSelectSchedulePageBehavior().scheduleMovie("2017-03-02 18:00", "1");
-		
+
+		getSelectSchedulePageBehavior().scheduleMovie("2017-03-02 17:00", "1");
+
 		List<String> seats = new ArrayList<String>();
 		seats.add("3,8");
-		
+
 		getSelectSeatsPageBehavior().selectSeats(seats);
 		getConfirmationPageBehavior().formatReservationMessage(seats);
-		
+
 		String uiMessage = getConfirmationPageBehavior().getReservationMessage();
-		
+
 		// Create expected message.
 		String expectedMessage = getConfirmationPageBehavior().formatReservationMessage(seats);
-		
+
 		Assert.assertEquals(uiMessage, expectedMessage, "Confirmation message");
-		
+
 		getConfirmationPageBehavior().getPage().getEndButton().click();
-		
+
 		// After transaction verification.
 		getSelectMoviePageBehavior().selectMovieByIndex(1);
-		getSelectSchedulePageBehavior().scheduleMovie("2017-03-02 18:00", "1");
+		getSelectSchedulePageBehavior().scheduleMovie("2017-03-02 17:00", "1");
 		getSelectSeatsPageBehavior().verifySeats(seats);
 	}
-
 
 	@AfterMethod
 	public void cleanUp() throws IOException {
 		EliteMovieCleanDataUtils.cleanAll();
 	}
-
 
 }
