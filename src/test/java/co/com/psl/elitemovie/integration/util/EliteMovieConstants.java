5ff7b7dd81fdca2e315885c1a8a0211b3dd197bd
Sebastian Velez <sebasv89@gmail.com>
2016-03-27 19:38:04 -0500

    changing ip to match docker-machine ip

--- src/test/java/co/com/psl/elitemovie/integration/util/EliteMovieConstants.java
+++ src/test/java/co/com/psl/elitemovie/integration/util/EliteMovieConstants.java
@@ -2,6 +2,6 @@ package co.com.psl.elitemovie.integration.util;
 
 public class EliteMovieConstants {
 
-	public static final String BASE_URL = "http://localhost:8080/elitemovie";
+	public static final String BASE_URL = "http://192.168.99.100:8080/elitemovie";
 
 }
--- src/test/java/co/com/psl/elitemovie/ui/test/EliteMovieTest.java
+++ src/test/java/co/com/psl/elitemovie/ui/test/EliteMovieTest.java
@@ -15,66 +15,57 @@ import co.com.psl.elitemovie.ui.action.SelectSeatsPageBehavior;
 
 public abstract class EliteMovieTest {
 
-	private static final String ELITE_MOVIE_MAIN_URL = "http://localhost:8080/elitemovie";
+	private static final String ELITE_MOVIE_MAIN_URL = "http://192.168.99.100:8080/elitemovie";
 	private SelectSchedulePageBehavior _selectSchedulePageBehavior;
 	private ConfirmationPageBehavior _confirmationPageBehavior;
 	private SelectMoviePageBehavior _selectMoviePageBehavior;
-	private SelectSeatsPageBehavior _selectSeatsPageBehavior; 
-	
+	private SelectSeatsPageBehavior _selectSeatsPageBehavior;
+
 	private static WebDriver _webDriver;
-	
-	public static WebDriver getWebDriver() 
-	{
+
+	public static WebDriver getWebDriver() {
 		return _webDriver;
 	}
 
 	@BeforeClass
-	public void init()
-	{
-		
+	public void init() {
+
 		_webDriver = new FirefoxDriver(new FirefoxProfile());
 		_webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		_webDriver.manage().window().maximize();
 		_webDriver.get(ELITE_MOVIE_MAIN_URL);
 	}
-	
+
 	@AfterClass
-	public void close()
-	{
+	public void close() {
 		_webDriver.close();
 	}
-	
-	public SelectMoviePageBehavior getSelectMoviePageBehavior() 
-	{
-		if(_selectMoviePageBehavior == null)
-		{
+
+	public SelectMoviePageBehavior getSelectMoviePageBehavior() {
+		if (_selectMoviePageBehavior == null) {
 			_selectMoviePageBehavior = new SelectMoviePageBehavior(getWebDriver());
 		}
-		
+
 		return _selectMoviePageBehavior;
 	}
 
-	public SelectSchedulePageBehavior getSelectSchedulePageBehavior() 
-	{
-		if(_selectSchedulePageBehavior == null)
-		{
+	public SelectSchedulePageBehavior getSelectSchedulePageBehavior() {
+		if (_selectSchedulePageBehavior == null) {
 			_selectSchedulePageBehavior = new SelectSchedulePageBehavior(getWebDriver());
 		}
 		return _selectSchedulePageBehavior;
 	}
-	
-	public SelectSeatsPageBehavior getSelectSeatsPageBehavior() 
-	{
-		if(_selectSeatsPageBehavior == null) {
+
+	public SelectSeatsPageBehavior getSelectSeatsPageBehavior() {
+		if (_selectSeatsPageBehavior == null) {
 			_selectSeatsPageBehavior = new SelectSeatsPageBehavior(getWebDriver());
 		}
 		return _selectSeatsPageBehavior;
 	}
-	
-public ConfirmationPageBehavior getConfirmationPageBehavior() {
-		
-		if(_confirmationPageBehavior == null)
-		{
+
+	public ConfirmationPageBehavior getConfirmationPageBehavior() {
+
+		if (_confirmationPageBehavior == null) {
 			_confirmationPageBehavior = new ConfirmationPageBehavior(getWebDriver());
 		}
 		return _confirmationPageBehavior;
